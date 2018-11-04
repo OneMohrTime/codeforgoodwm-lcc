d5ab744ffccb699faea3b152e717eff1737175ae
OneMohrTime <derek@onemohrti.me>
2018-11-03 23:11:52 -0400

    about

--- page.php
+++ page.php
@@ -15,7 +15,7 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area col-12">
+	<div id="primary" class="content-area container">
 		<main id="main" class="site-main">
 
 		<?php
@@ -30,13 +30,38 @@ get_header();
 			endif;
 
 		endwhile; // End of the loop.
-		?>
+		
+		if( have_rows('values_card') ):
+		echo '<h3>' . get_sub_field('title_copy') . '</h3>';
+		
+		while ( have_rows('values_card') ) : the_row();
+			// vars
+			$values_color = get_sub_field('color');
+			$values_title = get_sub_field('title');
+			$values_body = get_sub_field('body'); ?>
+
+			<div class="col-12 col-sm-6 col-md-4">
+			<div class="card <?php echo $values_color; ?>">
+				<div class="card-header">
+					<h3 class="card-title"><?php echo $values_title; ?></h3>
+				</div>
+				<div class="card-body">
+					<?php echo $values_body; ?>
+				</div>
+			</div>
+			</div>
 			
 		<?php
+		endwhile;
+		endif; ?>
+		
+		<div class="clearfix"></div>
 
+		<?php
 		// check if the repeater field has rows of data
 		if( have_rows('person') ): ?>
-			<div class="card-deck row">
+			<h3 class="my-3">Our Steering Committee</h3>
+			<div class="row">
 
 			<?php
 			// loop through the rows of data
@@ -48,9 +73,9 @@ get_header();
 				$description = get_sub_field('description'); ?>
 				
 				<div class="col-12 col-sm-6 col-md-4 col-lg-3">
-				<div class="card orange">
+				<div class="card bg-light text-dark pt-3 px-3 mb-3">
 					<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" class="card-img-top" />
-					<div class="card-body">
+					<div class="card-body px-0">
 						<h5 class="card-title"><?php echo $name; ?></h5>
 						<div class="card-text">
 							<?php echo $description; ?>
