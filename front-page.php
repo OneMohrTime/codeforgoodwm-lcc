ad78c6f17772fafa93c19c29af3c93888cd69abf
Michael Hurley <mhurley8594@gmail.com>
2018-11-03 20:05:50 -0400

    Updated front-page.php

--- front-page.php
+++ front-page.php
@@ -12,13 +12,13 @@ get_header(); ?>
 	</div>
 
 	<div class="container mt-3 mb-3">
-		<div class="col-lg-6">
+		<div class="col-lg-4">
 			<?php 
 			$intro = get_field('intro'); ?>
 			<h1><?php echo $intro['title']; ?></h1>
 			<?php echo $intro['content']; ?>
 		</div>
-		<div class="col-lg-6">
+		<div class="col-lg-8">
 			<?php 
 			$featured_card = get_field('featured_card'); ?>
 			<div class="card orange">
@@ -32,7 +32,7 @@ get_header(); ?>
 						</div>
 						<div class="col-lg-6">
 							<p class="text-right">
-								<a href="<?php echo $featured_card['button_link']; ?>" class="btn btn-light"><?php echo $featured_card['button_title']; ?></a>
+								<a href="<?php echo $featured_card['button_link']; ?>" class="btn btn-light active"><?php echo $featured_card['button_title']; ?></a>
 							</p>
 						</div>
 					</div>
@@ -41,34 +41,45 @@ get_header(); ?>
 		</div>
 	</div>
 
-	<div class="container">
+	<div class="container mt-3 mb-3 p-5 gray">
 	<?php if( have_rows('cards') ): ?>
 
 		<div class="card-deck">
 
-		<?php while( have_rows('cards') ): the_row(); 
+		<?php 
+        // Initialize the iterator.
+		$i = 0;
+		while( have_rows('cards') ): the_row(); 
 
 			// vars
 			$color = get_sub_field('color');
 			$content = get_sub_field('content');
 			$button_title = get_sub_field('button_title');
-			$button_link = get_sub_field('button_link'); ?>
-			
-			<div class="col-12 col-sm-6 col-md-4 col-lg-3">
-				<div class="card">
+			$button_link = get_sub_field('button_link'); 
+			?>
+				<div class="card <?php echo $color; ?>">
 					<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" class="card-img-top img-fluid" />
 					<div class="card-body">
 						<?php echo $content; ?>
 					</div>
-					<div class="card-footer">
-						<a href="<?php echo $button_link; ?>">
+					<div class="card-footer text-center <?php echo $color; ?>">
+						<a href="<?php echo $button_link; ?>" class="btn active">
 							<?php echo $button_title; ?>
 						</a>
 					</div>
 				</div>
-			</div>
 
-		<?php endwhile; ?>
+		<?php 
+		// Increment the iterator.
+		$i ++;
+
+		if ($i % 2 === 0) {
+			// Wrap card deck for small displays on every second card.
+			echo '<div class="w-100 d-none d-md-block d-sm-block d-lg-none m-2"></div>';
+		}
+
+		endwhile; 
+		?>
 
 		</div>
 
